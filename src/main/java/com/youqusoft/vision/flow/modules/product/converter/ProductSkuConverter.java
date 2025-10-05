54ec3130fde66d04787605b2df4b6a9fdf0e90cc
Jack.Zhang <whyloverjack@163.com>
2025-10-05 12:54:32 +0800

    1. 商品中增加SKU操作支持

--- src/main/java/com/youqusoft/vision/flow/modules/product/converter/ProductSkuConverter.java
+++ src/main/java/com/youqusoft/vision/flow/modules/product/converter/ProductSkuConverter.java
@@ -1,20 +1,38 @@
 package com.youqusoft.vision.flow.modules.product.converter;
 
-import org.mapstruct.Mapper;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.youqusoft.vision.flow.modules.product.model.entity.ProductSku;
+import com.youqusoft.vision.flow.modules.product.model.form.ProductSKUAttribute;
 import com.youqusoft.vision.flow.modules.product.model.form.ProductSkuForm;
+import com.youqusoft.vision.flow.modules.product.model.vo.ProductSkuVO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 /**
  * 商品SKU对象转换器
  *
  * @author youqusoft
- * @since 2025-10-05 09:55
+ * @since 2025-10-05
  */
 @Mapper(componentModel = "spring")
-public interface ProductSkuConverter{
+public interface ProductSkuConverter {
 
+    @Mapping(source = "skuAttributes", target = "skuAttributes", qualifiedByName = "jsonToProductSKUAttribute")
     ProductSkuForm toForm(ProductSku entity);
 
+    @Mapping(source = "skuAttributes", target = "skuAttributes", qualifiedByName = "productSKUAttributeToJson")
     ProductSku toEntity(ProductSkuForm formData);
+
+    @Mapping(source = "skuAttributes", target = "skuAttributes", qualifiedByName = "jsonToProductSKUAttribute")
+    ProductSkuVO toVO(ProductSku entity);
+
+    @Named("productSKUAttributeToJson")
+    default String productSKUAttributeToJson(ProductSKUAttribute productSKUAttribute) {
+        return productSKUAttribute.toJsonString();
+    }
+
+    @Named("jsonToProductSKUAttribute")
+    default ProductSKUAttribute jsonToProductSKUAttribute(String productSKUAttributeJson) {
+        return productSKUAttributeJson != null ? ProductSKUAttribute.fromJsonString(productSKUAttributeJson) : null;
+    }
 }
